2cb828042e51f322feb49b33b0174e2700f18691
unknown <adrianji1945@gmail.com>
2022-11-06 16:31:17 +0700

    last

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <h1 className="text-3xl font-medium mt-5 mb-5">Todolist</h1>
       <div className="flex">
         <input
-          className="input input-bordered w-full max-w-xs mr-3 shadow-md text-black"
+          className="input input-bordered w-full max-w-xs mr-3 shadow-sm shadow-slate-700"
           placeholder="e.g Finish Exercise"
           type="text"
           value={inputVal}
@@ -44,8 +44,8 @@ function App() {
           Add Task
         </button>
       </div>
-      <div className="mt-4 w-4/5 m-auto">
-        <h3 className="text-xl text-center">Your task</h3>
+        <h3 className="text-xl text-center mt-3">Your task</h3>
+      <div className="mt-4 w-4/5 m-auto border-t border-slate-300">
         {data && <div className="text-center font-medium mt-5">No Task In Here 🤔</div>}
         {inputArr.map((val, ind) => {
           return (
